0407cc0f1819fe722f1c7ad9305f76b5b292a57b
Hussein Al Hammad <husseinalhammad@gmail.com>
2020-01-15 15:33:23 +0400

    Update runtime.php

--- pipit/runtime.php
+++ pipit/runtime.php
@@ -313,3 +313,23 @@
 
         return false;
     }
+
+
+
+
+
+    /**
+     * Get the key of the collection an item belongs to
+     * 
+     * @param int $itemID
+     */
+    function pipit_get_collection_key_for($itemID) {
+        $API = new PerchAPI(1.0, 'pipit');
+        $DB = $API->get('DB');
+
+        $items_table = PERCH_DB_PREFIX . 'collection_items';
+        $collections_table = PERCH_DB_PREFIX . 'collections';
+        $itemID = $DB->pdb($itemID);
+        
+        return $DB->get_value("SELECT collectionKey FROM $collections_table  WHERE collectionID IN (SELECT collectionID from $items_table WHERE itemID=$itemID) LIMIT 1");
+    }
